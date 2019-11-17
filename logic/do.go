d8708ca630f2b4225e79790a76d3b8c8c82eee2f
jack33891024 <jack33891024@gmail.com>
2019-11-17 15:40:58 +0800

    seperator logic function

---
+++ logic/do.go
@@ -0,0 +1,103 @@
+package logic
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/dirmonitor/dal/db"
+	"github.com/dirmonitor/model"
+	"github.com/fsnotify/fsnotify"
+)
+
+const (
+	OP_CREATE = "创建文件"
+	OP_WRITE  = "写入文件"
+	OP_DELETE = "删除文件"
+	OP_RENAME = "重命名文件"
+	OP_CHMOD  = "修改权限"
+)
+
+func WatchEvent() {
+	//创建一个监控对象
+	watch, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watch.Close()
+	//添加要监控的对象，文件或文件夹
+	err = watch.Add(model.Directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//我们另启一个goroutine来处理监控对象的事件
+	go func() {
+		for {
+			select {
+			case ev := <-watch.Events:
+				{
+					if ev.Op&fsnotify.Create == fsnotify.Create {
+						record := &model.OpRecord{}
+						record.Operator = OP_CREATE
+						record.FilePath = ev.Name
+						err := db.InsertOperatorRecord(record)
+						if err != nil {
+							err = fmt.Errorf("create failed,err: %v\n", err)
+							return
+						}
+					}
+					if ev.Op&fsnotify.Write == fsnotify.Write {
+						//fmt.Println(nowStr, OP_WRITE, ev.Name)
+						record := &model.OpRecord{}
+						record.Operator = OP_WRITE
+						record.FilePath = ev.Name
+						err := db.InsertOperatorRecord(record)
+						if err != nil {
+							err = fmt.Errorf("write failed,err: %v\n", err)
+							return
+						}
+					}
+					if ev.Op&fsnotify.Remove == fsnotify.Remove {
+						//fmt.Println(nowStr, OP_DELETE, ev.Name)
+						record := &model.OpRecord{}
+						record.Operator = OP_DELETE
+						record.FilePath = ev.Name
+						err := db.InsertOperatorRecord(record)
+						if err != nil {
+							err = fmt.Errorf("delete failed,err: %v\n", err)
+							return
+						}
+					}
+					if ev.Op&fsnotify.Rename == fsnotify.Rename {
+						//fmt.Println(nowStr, OP_RENAME, ev.Name)
+						record := &model.OpRecord{}
+						record.Operator = OP_RENAME
+						record.FilePath = ev.Name
+						err := db.InsertOperatorRecord(record)
+						if err != nil {
+							err = fmt.Errorf("rename failederr: %v\n", err)
+							return
+						}
+					}
+					if ev.Op&fsnotify.Chmod == fsnotify.Chmod {
+						//fmt.Println(nowStr, OP_CHMOD, ev.Name)
+						record := &model.OpRecord{}
+						record.Operator = OP_CHMOD
+						record.FilePath = ev.Name
+						err := db.InsertOperatorRecord(record)
+						if err != nil {
+							err = fmt.Errorf("chmod failed,err: %v\n", err)
+							return
+						}
+					}
+				}
+			case err := <-watch.Errors:
+				{
+					log.Println("error : ", err)
+					return
+				}
+			}
+		}
+	}()
+	//循环
+	select {}
+}
