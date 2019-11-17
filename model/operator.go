9d5e70451f1a61aaa2974a74153eef9c31d8c18f
jack33891024 <jack33891024@gmail.com>
2019-11-17 12:49:05 +0800

    调整结构字段类型

--- model/operator.go
+++ model/operator.go
@@ -5,7 +5,7 @@ import "time"
 // OpRecord 记录目录变化的操作
 type OpRecord struct {
 	Id        int64     `db:"id"`         //自增id
-	EventTime time.Time `db:"event_time"` //事件记录事件
-	Operator  string    `db:"operator"`   //事件具体操作
-	FilePath  string    `db:"file_path"`  //事件对应的文件
+	EventTime time.Time `db:"event_time"` //事件时间
+	Operator  string    `db:"operator"`   //具体操作
+	FilePath  string    `db:"file_path"`  //对应文件
 }
